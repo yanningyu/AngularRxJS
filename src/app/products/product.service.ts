bb6c380ef4c7c184d4a477205680836c94cee7c2
Frankie Yu <frankie.yu@channelcentral.net>
2019-10-22 08:12:27 +0100

    added the frist commit

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -16,15 +16,22 @@ export class ProductService {
   private suppliersUrl = this.supplierService.suppliersUrl;
 
   constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+    private supplierService: SupplierService) { }
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
+  $products = this.http.get<Product[]>(this.productsUrl)
       .pipe(
+        map(products =>
+           products.map(
+             product => ({
+               ...product,
+               price: product.price  * 1.5,
+               searchKey: [product.productName]
+             }) as Product
+           )),
         tap(data => console.log('Products: ', JSON.stringify(data))),
         catchError(this.handleError)
       );
-  }
+  
 
   private fakeProduct() {
     return {
